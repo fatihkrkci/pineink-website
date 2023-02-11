98b709ce0146150889a5aefafb643795087d7592
Fatih Kürekçi <fatihkr02@gmail.com>
2023-02-12 02:14:28 +0300

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { DiscoverComponent } from './components/discover/discover.component';
+import { InitialComponent } from './components/home-page/components/initial/initial.component';
+import { AboutUsComponent } from './components/home-page/components/about-us/about-us.component';
+import { FeaturesComponent } from './components/home-page/components/features/features.component';
+import { FaqComponent } from './components/home-page/components/faq/faq.component';
+import { DownloadAppComponent } from './components/home-page/components/download-app/download-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashScreenComponent,
+    NaviComponent,
+    FooterComponent,
+    HomePageComponent,
+    ContactComponent,
+    DiscoverComponent,
+    InitialComponent,
+    AboutUsComponent,
+    FeaturesComponent,
+    FaqComponent,
+    DownloadAppComponent
   ],
   imports: [
     BrowserModule,
