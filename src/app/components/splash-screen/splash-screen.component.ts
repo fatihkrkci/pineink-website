e7334d527c04fe4248d2edc86db95e15f46318d1
Fatih Kürekçi <fatihkr02@gmail.com>
2023-02-12 16:32:28 +0300

    added splash-screen and pine-logo-black

--- src/app/components/splash-screen/splash-screen.component.ts
+++ src/app/components/splash-screen/splash-screen.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-splash-screen',
   templateUrl: './splash-screen.component.html',
   styleUrls: ['./splash-screen.component.css']
 })
-export class SplashScreenComponent {
+export class SplashScreenComponent implements OnInit {
+  windowWidth = 'string';
+  showSplash = true;
 
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.windowWidth = "-" + window.innerWidth + "px";
+
+      setTimeout(() => {
+        this.showSplash = !this.showSplash;
+      }, 500);
+    }, 1500);
+  }
 }
