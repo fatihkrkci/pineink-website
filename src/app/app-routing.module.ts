e303e20fc4ec625d61735d17657e158537c896aa
Fatih Kürekçi <fatihkr02@gmail.com>
2023-02-13 12:25:47 +0300

    completed contact page & updated footer, download, faq, navi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
+import { DiscoverComponent } from './components/discover/discover.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 
 const routes: Routes = [
-  {path: "", pathMatch:"full", component: HomePageComponent},
+  { path: "", pathMatch:"full", component: HomePageComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "discover", component: DiscoverComponent },
+  { path: "**", redirectTo: "", pathMatch: "full" }
 ];
 
 @NgModule({
