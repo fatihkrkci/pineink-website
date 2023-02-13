4a7a395251f40b512d88a4afc2283f4bfb4f6219
Fatih Kürekçi <fatihkr02@gmail.com>
2023-02-13 22:05:48 +0300

    bugs fixed

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,15 +1,30 @@
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
+        console.log("FAQ KALICAK!!!")
         new bootstrap.ScrollSpy(document.body, {
             target: '#mainNav',
-            offset: 74,
+            offset: 10000,
         });
     };
 
-    // Collapse responsive navbar when toggler is visible
+    var offset = 86;
+    let links = document.querySelectorAll('#navbarResponsive .nav-link');
+    console.log(links)
+    links.forEach(element => {
+        element.addEventListener('click',function ()
+        {
+            scrollOffset();
+        });
+    });
+
+    function scrollOffset() {
+        
+        $($(this).attr('href'))[0].scrollIntoView();
+        scrollBy(0, -offset);
+    }
+
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
